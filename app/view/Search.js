2775aa043442fc2701047cb6b3075ea9e5e72de1
Ganesh Raman <rgkooty@gmail.com>
2013-06-22 14:34:21 -0700

    added components to search page

--- app/view/Search.js
+++ app/view/Search.js
@@ -3,13 +3,58 @@ Ext.define('SenTripForce.view.Search', {
 	xtype : 'search',
 	
 	config : {
-		items : [ {
+		items : [
+		    // Select item to choose the location
+		    {
+            xtype: 'fieldset',
+            title: 'Select',
+            items: [
+                {
+                    xtype: 'selectfield',
+                    label: 'Location',
+                    options: [
+                        {text: 'Orlando',  value: 'orlando'},
+                        {text: 'Hawaii', value: 'hawaii'},
+                        {text: 'Las Vegas',  value: 'lasvegas'}
+                    ]
+                }
+            ]
+            },
+	        
+            // Start date field
+            {
+            xtype: 'fieldset',
+            items: [
+                {
+                    xtype: 'datepickerfield',
+                    label: 'Start',
+                    name: 'start',
+                    value: new Date()
+                }
+            ]
+            },
+        
+            // end date field
+            {
+            xtype: 'fieldset',
+            items: [
+                {
+                    xtype: 'datepickerfield',
+                    label: 'End',
+                    name: 'end',
+                    value: new Date()
+                }
+            ]
+            },
+        
+            // Go button
+		    {
 			xtype : 'button',
 			text : 'Go',
 			badgeText : 'Thanks!',
 			action: 'search'
-	
-		} ]
-
+			} 
+		
+		]
 	}
 });
